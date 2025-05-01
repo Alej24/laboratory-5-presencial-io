a091120d19c7667ec40be1402ba062ec4573bbec
Alej24 <99218281+Alej24@users.noreply.github.com>
2025-05-01 05:04:04 +0000

    Update main.c

--- Laboratory 5 presencial team/src/main.c
+++ Laboratory 5 presencial team/src/main.c
@@ -26,8 +26,10 @@ int current_target = -1;
 int last_click = -1;
 int timer_expired = -1;
 int win = 0;
+int timer_val = 3;
 
 pthread_mutex_t lock;
+
 int get_quadrant(int x, int y)
 {
   if (x < WIDTH / 2 && y < HEIGHT / 2)
@@ -41,46 +43,53 @@ int get_quadrant(int x, int y)
 
 void *mouse_listener(void *arg)
 {
-  // const char *device = "/dev/input/event2";
-  // int fd = open(device, O_RDONLY);
-  // if (fd == -1)
-  // {
-  //   perror("No se pudo abrir el dispositivo de entrada");
-  //   exit(1);
-  // }
+  const char *device = "/dev/input/event6";
+  int fd = open(device, O_RDONLY);
+  if (fd == -1)
+  {
+    perror("No se pudo abrir el dispositivo de entrada");
+    exit(1);
+  }
 
-  // struct input_event ev;
-  // int mx = 0, my = 0;
+  struct input_event ev;
+  int mx = 0, my = 0;
 
-  // while (1)
-  // {
-  //   if (read(fd, &ev, sizeof(ev)) == sizeof(ev))
-  //   {
-  //     if (ev.type == EV_ABS)
-  //     {
-  //       if (ev.code == ABS_X)
-  //         mx = ev.value;
-  //       if (ev.code == ABS_Y)
-  //         my = ev.value;
-  //     }
-  //     if (ev.type == EV_KEY && ev.code == BTN_LEFT && ev.value == 1)
-  //     {
-  //       int q = get_quadrant(mx / 10, my / 10);
-  //       pthread_mutex_lock(&lock);
-  //       last_click = q;
-  //       pthread_mutex_unlock(&lock);
-  //     }
-  //   }
-  // }
+  while (1)
+  {
+    if (read(fd, &ev, sizeof(ev)) == sizeof(ev))
+    {
+      if (ev.type == EV_ABS)
+      {
+        if (ev.code == ABS_X)
+          mx = ev.value;
+        if (ev.code == ABS_Y)
+          my = ev.value;
+      }
+      if (ev.type == EV_KEY && ev.code == BTN_LEFT && ev.value == 1)
+      {
+        int q = get_quadrant(mx / 10, my / 10);
+        pthread_mutex_lock(&lock);
+        last_click = q;
+        pthread_mutex_unlock(&lock);
+      }
+    }
+  }
 
-  // close(fd);
-  // return NULL;
+  close(fd);
+  return NULL;
 }
 
 int main()
 {
+  int rand_num = 0;
+
   pthread_t thread_io;
   pthread_t mouse_thread;
+
+  initscr();
+  keypad(stdscr, TRUE);
+  noecho();
+  curs_set(0);
 
   pthread_mutex_init(&lock, NULL);
   pthread_create(&mouse_thread, NULL, mouse_listener, NULL);
@@ -98,18 +107,26 @@ int main()
 
   timer_create(CLOCK_MONOTONIC, NULL, &timer_id);
 
-  initscr();
-  keypad(stdscr, TRUE);
-  noecho();
-  curs_set(0);
-
   while (1)
   {
     clear();
 
+    int row, col;
+
+    getmaxyx(stdscr, row, col);
+
     srand(time(NULL));
 
-    int rand_num = rand() % 4;
+    mvprintw(1, 0, "Timer: %d", timer_val);
+
+    timer_val--;
+
+    if (timer_val < 0) {
+      mvprintw((row / 2) - 1, (col / 2) - 5, "You Lose!");
+      timer_val = 3;
+      rand_num = rand() % 4;
+    }
+
     current_target = rand_num;
     last_click = -1;
     timer_expired = 0;
@@ -120,7 +137,7 @@ int main()
 
     // Timer parameters are configured
     // First shot of the timer
-    timer.it_value.tv_sec = 3;
+    timer.it_value.tv_sec = 1;
     timer.it_value.tv_nsec = 0;
     // Is 0 because it would be periodic otherwise
     timer.it_interval.tv_sec = 0;
@@ -143,10 +160,10 @@ int main()
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1E9;
 
-    // mvprintw(5, 5, "%f", elapsed_time);
     refresh();
   }
 
+  pthread_join(mouse_thread, NULL);
   pthread_join(thread_io, NULL);
 
   getch();
