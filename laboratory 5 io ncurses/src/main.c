f257410b6b2c6a37dc6fccdd5605addbf4c220b3
Alej24 <99218281+Alej24@users.noreply.github.com>
2025-05-05 03:31:57 +0000

    Update main.c

--- laboratory 5 io ncurses/src/main.c
+++ laboratory 5 io ncurses/src/main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <ctype.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -7,20 +8,100 @@
 #include <unistd.h>
 #include <string.h>
 #include "lab5io.h"
-#include <curses.h>
+#include <ncurses.h>
 #include <time.h>
 #include <signal.h>
 #include <sys/resource.h>
 #include <sys/signal.h>
 #include <wait.h>
 #include <pthread.h>
+#include <linux/input.h>
 
 #define ENTER_NCURSES 10
 
 int stop_timer = 0;
+int current_target = -1;
+int last_click = -1;
+int win = 0;
+int timer_val = 3;
+int row, col;
 
-int main() {
+pthread_mutex_t lock;
+
+// int get_quadrant(int x, int y)
+// {
+//   if (x < col / 2 && y < row / 2)
+//     return 0;
+//   if (x >= (col / 2) && y < row / 2)
+//     return 1;
+//   if (x < col / 2 && y >= (row / 2))
+//     return 2;
+//   return 3;
+// }
+
+// void *mouse_listener(void *arg)
+// {
+//   const char *device = "/dev/input/event6";
+//   int fd = open(device, O_RDONLY);
+//   if (fd == -1) {
+//     perror("Cannot open input device");
+//     exit(1);
+//   }
+
+//   struct input_event ev;
+
+//   double mouse_x = 32767.0;
+//   double mouse_y = 32767.0;
+
+//   const double sensitivity = 200.0;  
+
+//   while (1) {
+//     if (read(fd, &ev, sizeof(ev)) == sizeof(ev)) {
+//       if (ev.type == EV_REL) {
+//         if (ev.code == REL_X) {
+//           mouse_x += ev.value * sensitivity;
+//           if (mouse_x < 0) mouse_x = 0;
+//           if (mouse_x > 65535) mouse_x = 65535;
+//         }
+//         if (ev.code == REL_Y) {
+//           mouse_y += ev.value * sensitivity;
+//           if (mouse_y < 0) mouse_y = 0;
+//           if (mouse_y > 65535) mouse_y = 65535;
+//         }
+//       }
+
+//       if (ev.type == EV_KEY && ev.code == BTN_LEFT && ev.value == 1) {
+//         pthread_mutex_lock(&lock);
+//         getmaxyx(stdscr, row, col);
+
+//         int terminal_x = (int)(mouse_x * col / 65535.0);
+//         int terminal_y = (int)(mouse_y * row / 65535.0);
+
+//         last_click = get_quadrant(terminal_x, terminal_y);
+
+//         pthread_mutex_unlock(&lock);
+//       }
+//     }
+//   }
+
+//   close(fd);
+//   return NULL;
+// }
+
+int main()
+{
+  int rand_num = 0;
+
   pthread_t thread_io;
+  pthread_t mouse_thread;
+
+  initscr();
+  keypad(stdscr, TRUE);
+  noecho();
+  curs_set(0);
+
+  pthread_mutex_init(&lock, NULL);
+  pthread_create(&mouse_thread, NULL, mouse_listener, NULL);
 
   // Sets the attributes of the thread.
   pthread_attr_t attr;
@@ -35,26 +116,39 @@ int main() {
 
   timer_create(CLOCK_MONOTONIC, NULL, &timer_id);
 
-  initscr();
-  keypad(stdscr, TRUE);
-  noecho();
-  curs_set(0);
-
-  while (1) {
+  while (1)
+  {
     clear();
-    
+
+    getmaxyx(stdscr, row, col);
+
     srand(time(NULL));
 
-    int rand_num = rand() % 4;
+    mvprintw(1, 0, "Timer: %d", timer_val);
 
-    pthread_create(&thread_io, &attr, ioDevices, (void*)&rand_num);
+    timer_val--;
+
+    if (timer_val < 0 && last_click != current_target) {
+      mvprintw((row / 2) - 1, (col / 2) - 5, "You Lose!");
+      timer_val = 3;
+      rand_num = rand() % 4;
+    } else if (timer_val >= 0 && last_click == current_target) {
+      mvprintw((row / 2) - 1, (col / 2) - 5, "You Win!");
+      timer_val = 3;
+      rand_num = rand() % 4;
+    }
+
+    current_target = rand_num;
+    last_click = -1;
+
+    pthread_create(&thread_io, &attr, ioDevices, (void *)&rand_num);
 
     struct itimerspec timer;
 
     // Timer parameters are configured
     // First shot of the timer
     timer.it_value.tv_sec = 1;
-    timer.it_value.tv_nsec =0;
+    timer.it_value.tv_nsec = 0;
     // Is 0 because it would be periodic otherwise
     timer.it_interval.tv_sec = 0;
     timer.it_interval.tv_nsec = 0;
@@ -67,18 +161,19 @@ int main() {
     // The timer starts
     timer_settime(timer_id, 0, &timer, NULL);
 
-    while (!stop_timer) {
+    while (!stop_timer)
+    {
       pause();
     }
 
     // Gets the time after the timer ends and calculates the time it took from start to finish.
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1E9;
-    
-    // mvprintw(5, 5, "%f", elapsed_time);
+
     refresh();
   }
 
+  pthread_join(mouse_thread, NULL);
   pthread_join(thread_io, NULL);
 
   getch();
@@ -87,6 +182,7 @@ int main() {
   return 0;
 }
 
-void TimerHandler(int param) {
+void TimerHandler(int param)
+{
   stop_timer = 1;
 }
