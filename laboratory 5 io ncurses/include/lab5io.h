515069fcc71dca5ac886dd917390f5b1548623cf
Alej24 <99218281+Alej24@users.noreply.github.com>
2025-05-05 03:30:57 +0000

    Update lab5io.h

--- laboratory 5 io ncurses/include/lab5io.h
+++ laboratory 5 io ncurses/include/lab5io.h
@@ -1,8 +1,19 @@
 #ifndef LAB5IO_H
 #define LAB5IO_H 
 
+extern int stop_timer;
+extern int current_target;
+extern int last_click;
+extern int win;
+extern int timer_val;
+extern int row, col;
+
 void* ioDevices(void* params);
 
 void TimerHandler(int param);
+
+int get_quadrant(int x, int y);
+
+void *mouse_listener(void *arg);
 
 #endif // LAB5IO_H
